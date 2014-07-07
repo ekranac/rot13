2b877ffc5804fcfebf4eadad9d33550164a8be52
ekranac <besal.ziga@gmail.com>
2014-07-07 15:05:07 +0200

    template render

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
-form = """
-<form method="post">
-<h1>ROT13</h1>
-<textarea style="height:300px; width:500px;" type="text" name="text">%(value)s</textarea>
-<input type="submit">
-<br>
-</form>
-"""
 
-class MainPage(webapp2.RequestHandler):
 
-    def write_form(self, value=""):
-        self.response.out.write(form %{"value":value})
+class BaseHandler(webapp2.RequestHandler):
+    def render_template(self, view_filename, params=None):
+		if not params:
+			params = {}
+		path = os.path.join(os.path.dirname(__file__), 'templates', view_filename)
+		self.response.out.write(template.render(path, params))
 
+
+
+
+class MainPage(BaseHandler):
     def get_value(self):
         value=  self.request.get("text")
         value = list(value)
@@ -25,6 +26,7 @@ class MainPage(webapp2.RequestHandler):
         cAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         converted_value=""
         for element in list_value:
+
             if element.islower():
                 indeks = alphabet.index(element)
                 if indeks >= 13 and indeks <= 25:
@@ -32,7 +34,7 @@ class MainPage(webapp2.RequestHandler):
                 else:
                     indeks = indeks + 13
                 converted_value = converted_value + alphabet[indeks]
-                
+
             if element.isupper():
                 indeks = cAlphabet.index(element)
                 if indeks >= 13 and indeks <= 25:
@@ -40,16 +42,21 @@ class MainPage(webapp2.RequestHandler):
                 else:
                     indeks = indeks + 13
                 converted_value = converted_value + cAlphabet[indeks]
+
+
+            
         return converted_value
 
+
     def get(self):
-        self.write_form()
+        self.render_template("index.html")
 
     def post(self):
         listed_value = self.get_value()
         stavek = self.convert_value(listed_value)
 
-        self.write_form(stavek)
+        params = {"value":stavek}
+        self.render_template("index.html", params)
 
 
 
